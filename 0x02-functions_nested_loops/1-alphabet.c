50d83b4609d1b3cf2275ef1da3791df4d3c70c5f
Ayomide Emmanuel Adeyeye <ayomideadeyeyeyee1@gmail.com>
2026-09-14 18:07:25 +0100

    fix error in  alphabet task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet - prints the alphabet in lowercase, followed
+ * by a new line
  */
 void print_alphabet(void)
 {
-	char letter;
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar(letter);
-	}
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
 	_putchar('\n');
 }
